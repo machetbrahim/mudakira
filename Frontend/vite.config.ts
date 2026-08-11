f0fc81219917e399f0486011f1552d6371efcbe2
machetbrahim <machetbrahim1980@gmail.com>
2026-08-11 15:32:30 -0400

    Update vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // مسارات نسبية
+  build: {
+    outDir: 'docs', // نضع المخرجات في مجلد docs
+    emptyOutDir: true,
+  },
   server: {
     port: 3000,
   },
